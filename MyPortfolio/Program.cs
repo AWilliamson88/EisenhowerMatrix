34188aabd16b0e36d835dd4e51a3c5b3a523cb4b
AWilliamson88 <87967861+AWilliamson88@users.noreply.github.com>
2022-05-09 13:56:43 +0800

    Add local storage todo list plus remove duplicate folder

--- MyPortfolio/Program.cs
+++ MyPortfolio/Program.cs
@@ -10,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<UIToDoListService>();
+builder.Services.AddScoped<LocalStorageService>();
+builder.Services.AddScoped<DefaultDataService>();
 builder.Services.AddHttpClient<IUIToDoListService, UIToDoListService>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:7127/");
